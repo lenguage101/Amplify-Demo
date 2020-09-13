61f6cec14fb2addfc812d15025625487938691d4
Tommy Leng <tommyleng@yahoo.com>
2020-09-13 16:09:30 -0700

    updated index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import Amplify from "aws-amplify";
-import config from "./aws-exports.js";
-Amplify.configure(config);
+import awsmobile from "./aws-exports.js";
+Amplify.configure(awsmobile);
 
 ReactDOM.render(
   <React.StrictMode>
